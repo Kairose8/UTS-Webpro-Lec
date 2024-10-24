01e8cd0e974241438473d2496b64c3af3416bd17
xdominicman <136241335+xdominicman@users.noreply.github.com>
2024-10-25 01:37:19 +0700

    finished process

--- user_registration&authentication/admin_login_process.php
+++ user_registration&authentication/admin_login_process.php
@@ -4,3 +4,25 @@ session_start();
 
 // Include database connection
 include "../db_conn.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username = :username";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(":username", $username);
+$stmt->execute();
+
+$user - $stmt-> fetch();
+
+if ($user) {
+    if (password_verify($password, $user['$password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header("Location: admin-dashboard-index.php");
+        exit;
+    } else {
+        echo "Invalid Password.";
+    }
+}
+?>
